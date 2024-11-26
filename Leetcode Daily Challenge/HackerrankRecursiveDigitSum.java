dc2ba7b8c3460b9bffe464529a8399503f022420
SureshKumar1710 <89594101+SureshKumar1710@users.noreply.github.com>
2024-11-26 12:26:11 +0530

    57

---
+++ Leetcode Daily Challenge/HackerrankRecursiveDigitSum.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'superDigit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING n
+     *  2. INTEGER k
+     */
+    public static String helper(String n)
+    {
+        long ans=0;
+        for(int i=0;i<n.length();i++)
+        {
+            ans+=(n.charAt(i)-'0');
+        }
+        return ""+ans;
+    } 
+    public static int superDigit(String n, int k) 
+    {
+        String temp=Long.parseLong(helper(n))*k+"";
+        while(temp.length()!=1)
+        {
+            temp=helper(temp);
+        }
+        return Integer.parseInt(temp);
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        String n = firstMultipleInput[0];
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.superDigit(n, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
