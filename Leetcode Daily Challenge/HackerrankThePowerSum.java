ac0af107a7ca9948958d14f662327d8f3da8edbd
SureshKumar1710 <89594101+SureshKumar1710@users.noreply.github.com>
2024-12-06 15:16:33 +0530

    Create HackerrankThePowerSum.java

---
+++ Leetcode Daily Challenge/HackerrankThePowerSum.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'powerSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER X
+     *  2. INTEGER N
+     */
+    static int res=0;
+    public static int powerSum(int X, int N) {
+    // Write your code here
+         rec(1,X,N);
+         return res;
+    }
+    
+    public static void rec(int start,int target, int power)
+    {
+        if(target<0)
+            return;
+        if(target==0)
+        {
+            res++;
+            return;
+        }
+        for(int i=start;i*i<=target;i++)
+        {
+            rec(i+1,target-(int)Math.pow(i,power),power);
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int X = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.powerSum(X, N);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
